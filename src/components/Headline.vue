1dc61296c587c725a527c6d0138d6545a5d0ad39
Soumitha <you@example.com>
2022-11-29 16:59:20 +0530

    updated text

--- src/components/Headline.vue
+++ src/components/Headline.vue
@@ -5,7 +5,7 @@
       <br />
       for everyone
     </h1>
-    <h2 class="text-3xl font-light">Find your next job at bright corp</h2>
+    <h2 class="text-3xl font-light">your next job at bright corp</h2>
   </section>
 </template>
 <script>
