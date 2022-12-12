03227d1394e26e441df21042ea0d4a071b908ec7
Soumitha <you@example.com>
2022-12-12 18:01:02 +0530

    dynamic

--- src/components/JobResults/JobListing.vue
+++ src/components/JobResults/JobListing.vue
@@ -1,17 +1,25 @@
 <template>
   <li class="mb-7">
-    <router-link to="/jobs/results/1" class="jobsoutline">
+    <router-link :to="jobPageLink" class="jobsoutline">
       <div class="pt-5 pb-2 mx-8 border-b border-solid border-brand-gray-2">
         <h2 class="mb-2 text-2xl">
-          Techinal Program Manager, Perception,Augmented Reality
+          {{ job.title }}
         </h2>
         <div class="flex flex-row align-middle">
           <div class="mr-5">
-            <span>Bright</span>
+            <span>{{ job.organization }}</span>
           </div>
 
           <div>
-            <span>San Francisco,CA,USA</span>
+            <ul>
+              <li
+                v-for="location in job.locations"
+                :key="location"
+                class="inline-block mr-5"
+              >
+                {{ location }}
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -20,17 +28,17 @@
           <h3 class="mt-1 mb-2">Qualifications</h3>
           <div>
             <ul class="pl-8 list-disc">
-              <li>Bachelor's degree or equivalent practice experience.</li>
-              <li>5 years of expireance in program management</li>
-              <li>
-                Expireance analyzing data through querying database(e.g.
-                SQL),using spreadsheet software,& creating statistical models
+              <li
+                v-for="qualification in job.minimumQualifications"
+                :key="qualification"
+              >
+                {{ qualification }}
               </li>
             </ul>
           </div>
         </div>
         <div class="mt-2 text-center">
-          <router-link to="/jobs/results/1" class="text-brand-blue-1"
+          <router-link :to="jobPageLink" class="text-brand-blue-1"
             >Expand</router-link
           >
         </div>
@@ -41,6 +49,17 @@
 <script>
 export default {
   name: "JobListing",
+  props: {
+    job: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    jobPageLink() {
+      return `/jobs/results/${this.job.id}`;
+    },
+  },
 };
 </script>
 
