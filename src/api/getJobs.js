f2220b63f754c8f8362c2e0fcb8fe437448b2e05
Soumitha <you@example.com>
2022-12-23 12:35:07 +0530

    api

--- src/api/getJobs.js
+++ src/api/getJobs.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const getJobs = async () => {
-  const baseUrl = process.env.VUE_APP_API_URL;
-  const response = await axios.get(`${baseUrl}/jobs`);
+  // const baseUrl = process.env.VUE_APP_API_URL;
+  // const response = await axios.get(`${baseUrl}/jobs`);
+  const response = await axios.get("http://localhost:3000/jobs");
   return response.data;
 };
 
